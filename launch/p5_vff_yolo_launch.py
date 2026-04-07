d6d7eb4bde42c766413d85bc0607e861d0e3863f
dafauu <dafaucillox2005@gmail.com>
2026-04-07 10:47:14 +0200

    started coding launcher

--- launch/p5_vff_yolo_launch.py
+++ launch/p5_vff_yolo_launch.py
@@ -1,3 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
 
+def generate_launch_description():
+
+	return LaunchDescription([
+		
+	])
